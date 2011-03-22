0d95f546cdcc6ea5339d164942fac679efd95fe2
David Haslem <therabidbanana@gmail.com>
2011-03-22 11:42:01 -0400

    Update options

--- src/helpers/j_query_fallback_helper.rb
+++ src/helpers/j_query_fallback_helper.rb
@@ -1,15 +1,15 @@
 module JQueryFallbackHelper
   def jquery_with_fallback(options={})
-    return local_jquery(options) if CONFIG.paranoia
-    options = {:version => "1.5.1", :absolute => false}.merge(options)
+    options = {:jquery_version => "1.5.1", :absolute_links => false, :paranoia =>false}.merge(options)
+    return local_jquery(options) if options[:paranoia]
     str = ""
-    str += "<script src=\"//ajax.googleapis.com/ajax/libs/jquery/#{options[:version]}/jquery.js\"></script>"
-    url = (options[:absolute]? '/' : '')+ 'javascripts/libs/jquery.min.js'
+    str += "<script src=\"//ajax.googleapis.com/ajax/libs/jquery/#{options[:jquery_version]}/jquery.js\"></script>"
+    url = (options[:absolute_links]? '/' : '')+ 'javascripts/libs/jquery.min.js'
     str += "\n<script>window.jQuery || document.write(\"<script src='#{url}'>\\x3C/script>\")</script>"
   end
 
   def local_jquery(options={})
-    url = (options[:absolute]? '/' : '')+ 'javascripts/libs/jquery.min.js'
+    url = (options[:absolute_links]? '/' : '')+ 'javascripts/libs/jquery.min.js'
     "<script src='#{url}'></script>"
   end
 end
