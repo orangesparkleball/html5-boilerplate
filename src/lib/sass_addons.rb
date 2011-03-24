488b80ab4c271cb35fb49db2d2ce8a6c978d56ea
David Haslem <therabidbanana@gmail.com>
2011-03-24 11:24:06 -0400

    Convert angle to rad

--- src/lib/sass_addons.rb
+++ src/lib/sass_addons.rb
@@ -1,9 +1,12 @@
 require 'sass'
 module Sass::Script::Functions
+  DEGTORAD = Math.PI * 2 / 360;
   def sin(angle)
+    angle = angle*DEGTORAD
     Sass::Script::Parser.parse(Math.sin(angle.value).to_s, 0, 0)
   end
   def cos(angle)
+    angle = angle*DEGTORAD
     Sass::Script::Parser.parse(Math.cos(angle.value).to_s, 0, 0)
   end
 end
